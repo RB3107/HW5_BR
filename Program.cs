71e7f7f2fca899ea7c6fa35c2a884a887a9f59f9
Raisa01~ <zarvirova@mail.ru>
2022-11-26 22:39:38 +0300

    Task 36

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-int size = 10;
+/*int size = 10;
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
@@ -26,6 +26,34 @@ void FillArrayRandomNumbers(int[] array)
         array[i] = new Random().Next(1, 555);
     }
 }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}*/
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sumNumbersEvenIndex = 0;
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sumNumbersEvenIndex += numbers[i];
+}
+Console.Write(sumNumbersEvenIndex);
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
 void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
